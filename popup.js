4ec683930833cd29315d851c819dc66cdd9a0862
uesteibar <uesteibar@gmail.com>
2015-08-01 14:11:20 +0200

    working search

--- popup.js
+++ popup.js
@@ -1,10 +1,21 @@
 $(document).ready(function() {
+  var baseUrl = 'http://localhost:3000';
   $('#checkbook').on('click', function(event) {
     event.preventDefault();
     var bookname = $('#bookname').val();
-    var request = $.get('http://localhost:3000/api/books/search/' + bookname);
+    var request = $.get(baseUrl + '/api/books/search/' + bookname);
     request.done(function(data) {
       console.log(data);
+      var html = "";
+      if (data.length > 0) {
+        html = '<h3>We have that!</h3>';
+        html += '<h2>' + data[0].title + '</h2>';
+        html += '<h3>' + data[0].author + '</h3>';
+        html += '<a href="' + baseUrl + '/books/' + data[0].id + '" target="_blank">Ask for it!</a>';
+      } else {
+        html = '<h3>Sorry, we don\'t have that! </h3>';
+      }
+      $('#bookcontainer').html(html);
     });
   });
 });
