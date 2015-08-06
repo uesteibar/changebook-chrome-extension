f4807e1824c4a956b362a4c6f2e510ed700aa228
uesteibar <uesteibar@gmail.com>
2015-08-06 13:12:20 +0200

    scrapper working

--- popup.js
+++ popup.js
@@ -2,20 +2,31 @@ $(document).ready(function() {
   var baseUrl = 'http:changebook.herokuapp.com';
   $('#checkbook').on('click', function(event) {
     event.preventDefault();
-    var bookname = $('#bookname').val();
-    var request = $.get(baseUrl + '/api/v1/books/complex_search/', {term: bookname});
-    request.done(function(data) {
-      console.log(data);
-      var html = "";
-      if (data.length > 0) {
-        html = '<h3>We have that!</h3>';
-        html += '<h2>' + data[0].title + '</h2>';
-        html += '<h3>' + data[0].author + '</h3>';
-        html += '<a href="' + baseUrl + '/books/' + data[0].id + '" target="_blank">Ask for it!</a>';
-      } else {
-        html = '<h3>Sorry, we don\'t have that! </h3>';
-      }
-      $('#bookcontainer').html(html);
+    // var bookname = $('#bookname').val();
+    function checkBook(bookname) {
+      console.log(bookname);
+      var request = $.get(baseUrl + '/api/v1/books/complex_search/', {
+        term: bookname
+      });
+      request.done(function(data) {
+        console.log(data);
+        var html = "";
+        if (data.length > 0) {
+          html = '<h3>We have that!</h3>';
+          html += '<h2>' + data[0].title + '</h2>';
+          html += '<h3>' + data[0].author + '</h3>';
+          html += '<a href="' + baseUrl + '/books/' + data[0].id + '" target="_blank">Ask for it!</a>';
+        } else {
+          html = '<h3>Sorry, we don\'t have that! </h3>';
+        }
+        $('#bookcontainer').html(html);
+      });
+    }
+
+    chrome.tabs.getSelected(null, function(tab) {
+      chrome.tabs.sendMessage(tab.id, {
+        message: "scrap!"
+      }, checkBook);
     });
   });
 });
