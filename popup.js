011c147ad13ebc3d2b8c2bd8e1e17fe659e069b5
uesteibar <uesteibar@gmail.com>
2015-08-06 18:52:14 +0200

    add loading spinner

--- popup.js
+++ popup.js
@@ -3,6 +3,7 @@ $(document).ready(function() {
   var button = $('#checkbook');
   button.on('click', function(event) {
     event.preventDefault();
+    $('#bookcontainer').html('<div class="loading"></div>')
 
     function checkBook(bookname) {
       var request = $.get(baseUrl + '/api/v1/books/complex_search/', {
@@ -11,12 +12,12 @@ $(document).ready(function() {
       request.done(function(data) {
         var html = "";
         if (data.length > 0) {
-          html = '<h3>We have that!</h3>';
-          html += '<h2>' + data[0].title + '</h2>';
-          html += '<h3>' + data[0].author + '</h3>';
-          html += '<a href="' + baseUrl + '/books/' + data[0].id + '" target="_blank">Get it for free!</a>';
+          html = '<h2>We have that!</h2>';
+          html += '<h1>' + data[0].title + '</h1>';
+          html += '<h2>' + data[0].author + '</h2>';
+          html += '<h2><a href="' + baseUrl + '/books/' + data[0].id + '" target="_blank">Ask for it here!</a></h3>';
         } else {
-          html = '<h3>Sorry, we don\'t have that! </h3>';
+          html = '<h2>Sorry, we don\'t have that! </h2>';
         }
         $('#bookcontainer').html(html);
       });
